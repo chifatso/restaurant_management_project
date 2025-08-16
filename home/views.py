9458fb9dc78201b8bbc407c6f7bad488ab935627
chifatso <156987629+chifatso@users.noreply.github.com>
2025-08-16 14:17:40 +0200

    32868

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.conf import settings
+from rest_framework.decorators import api_views
+from rest_framework.response import response
+from .models import Restaurant
 
 # Create your views here.
 def homepage(request):
