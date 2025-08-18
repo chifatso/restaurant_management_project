a0c3af2a70e451b00a00a4e2481913d757d2b479
chifatso <156987629+chifatso@users.noreply.github.com>
2025-08-18 15:04:32 +0200

    32862

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from rest_framework.decorators import api_views
 from rest_framework.response import response
 from .models import Restaurant
-from django.db import DatabaseError #to handle DB-specific errors
+from django.db.utils import DatabaseError #to handle DB-specific errors
 
 
 # Create your views here.
@@ -13,11 +13,16 @@ def homepage(request):
         #Try and get restaurant name from database
         restaurant = Restaurant.objects.first()
         restaurant_name = restaurant.name if restaurant else settings.restaurant_name
+        restaurant_phone = restaurant.phone if restaurant else settings.restaurant_phone
     except DatabaseError:
         #if DB connection/query fails use a fallback
         restaurant_name = settings.restaurant_name
+        restaurant_phone = settings.restaurant_phone
 
-    return render(request, "home/index.html", {"restaurant_name": restaurant_name})
+    return render(request, "home/index.html", {
+        "restaurant_name": restaurant_name,
+        "restaurant_phone": restaurant_phone,
+        })
 
 def about(request):
     try:
