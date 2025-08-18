bd4e936ce2d08dace20b9b3df4c14a2c5a1f3686
chifatso <156987629+chifatso@users.noreply.github.com>
2025-08-18 14:54:12 +0200

    32864

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path("", views.homepage, name="home"),
+    path("about/", views.about, name="about"),
+    path("reservations/", views.reservations, name="reservations"),
     
 ]
