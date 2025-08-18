8902e276e391d68ab733512372bb932f510bbc98
chifatso <156987629+chifatso@users.noreply.github.com>
2025-08-18 15:42:16 +0200

    32861

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.homepage, name="home"),
     path("about/", views.about, name="about"),
     path("reservations/", views.reservations, name="reservations"),
+    path("contact/", views.contact, name="contact"),
     
 ]
