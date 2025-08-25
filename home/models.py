2ec688b903031bb079efedf5c5c755edd3402dd7
chifatso <156987629+chifatso@users.noreply.github.com>
2025-08-25 12:29:17 +0200

    37705

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+    name = models.CharField(max_length=100) #Optional: name of the customer
+    email = models.EmailField(blank = True) #Optional: all blank emails
+    comments = models.TextField() #Feedback text
+    created_at = models.DateTimeField(auto_now_add = True) #Timestamp
+
+    def __str__(self):
+        return f"Feedback from {self.name or 'Anonymous'}"
