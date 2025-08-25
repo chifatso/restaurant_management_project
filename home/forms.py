bbb149e7646ab9a4450f21f5e4fe9d6913f01769
chifatso <156987629+chifatso@users.noreply.github.com>
2025-08-25 12:33:35 +0200

    37705

--- home/forms.py
+++ home/forms.py
@@ -1,2 +1,10 @@
 from django import forms
-from .models import Feed
+from .models import Feedback
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['name', 'email', 'comments'] #Fields to display
+        widgets = {
+            'comments':forms.Textarea(attrs={'rows':4, 'placeholder':'Write your feedback here.'})
+        }
