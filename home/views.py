19b4c0e4d7d7c9b70517a91251f16373a08172c3
chifatso <156987629+chifatso@users.noreply.github.com>
2025-08-29 12:15:40 +0200

    40484

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.conf import settings
-from rest_framework.decorators import api_views
-from rest_framework.response import response
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Restaurant
-from django.db.utils import DatabaseError #to handle DB-specific errors
+from django.db.utils import DatabaseError #to handle DB-specific error
+from .serializers import MenuItemSerializer
 
 
 # Create your views here.
@@ -34,3 +35,23 @@ def about(request):
 
 def reservations(request):
     return render(request, "home/reservations.html")
+
+@api_view(["GET"]) #This API only supports get requests
+def menu_list(request):
+    """
+    API endpoint that returns the restaurant's menu.
+    For now, we use a hardcoded list of menu items
+
+    """
+    #Hardcoded menu (will later be replaced with DB query like Menu.objects.all())
+    menu = [
+        {"name":"Margherita Pizza", "description":"Classic pizza with cheese and tomato.", "price":8.99},
+        {"name":"Caesar Salad", "description":"Crisp romaine with Caesar dressing and croutons.", "price":5.49},
+        {"name":"Spaghetti Bolognese", "description":"Traditional pasta with rich meat sauce.", "price":10.99}
+    ]
+
+    #Serialize menu data -> JSON
+    serializer = MenuItemSerializer(menu, many = True)
+
+    #Return JSON response
+    return Response(serializer.data)
