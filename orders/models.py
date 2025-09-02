71ef10171fd28ecb2864671a0e4d4e0dd25e2394
chifatso <156987629+chifatso@users.noreply.github.com>
2025-09-02 13:27:07 +0200

    40482

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from products.models import Menu 
 
 # Create your models here.
+class Order(models.Model):
+    """Represents a customer order"""
+    customer_name = models.CharField(max_length = 100) #Using CharField for limited character length
+    menu_item = models.ForeignKey(Menu, on_delete = models.CASCADE) #If Menu is deleted orders are deleted too.
+    quantity = models.PositiveIntegerField(default = 1)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"Order #{self.id}-{self.customer_name}"
+
