228d55f09119b321d9b99b277600c2295892d9ed
chifatso <156987629+chifatso@users.noreply.github.com>
2025-09-02 13:40:38 +0200

    40483

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Order
 
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("customer_name", "menu_item", "quantity", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("customer_name",)
+
