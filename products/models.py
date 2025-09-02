7bfa64269e1a939757dff45da7c705dde86db1f8
chifatso <156987629+chifatso@users.noreply.github.com>
2025-09-02 13:30:31 +0200

    40482

--- products/models.py
+++ products/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class Item(models.Model):
-    item_name = models.CharField(max_length=150)
-    item_price = models.DecimalField(max_digits=10, decimal_places=2)
+class Menu(models.Model):
+    """Represents a dish or Item available in the restaurant"""
+    name = models.CharField(max_length=150) #CharField for limited characters
+    description = models.TextField() #Textfield for longer text
+    price = models.DecimalField(max_digits=10, decimal_places=2) #Like 199.22
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.item_name)
+        #What will display in the admin panels dropdowns or lists
+        return str(self.name)
