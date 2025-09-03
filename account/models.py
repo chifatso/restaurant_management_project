c617c258ea2218be599d1481ce3bfe8958ae9313
chifatso <156987629+chifatso@users.noreply.github.com>
2025-09-03 11:38:03 +0200

    40480

--- account/models.py
+++ account/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 # Extend the built-in user model with extra fields.
@@ -13,3 +15,14 @@ class Profile(models.Model):
     def __str__(self):
         # Display username when looking at this object in admin panel.
         return self.user.username
+
+# Create profile automatically when a new User is created.
+@receiver(post_save, sender = User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user = instance)
+
+# Save profile when User is saved
+@receiver(post_save, sender = User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
