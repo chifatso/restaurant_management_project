bbc655a7d42b0535d9ec319ab2d185d30769b220
chifatso <156987629+chifatso@users.noreply.github.com>
2025-09-03 11:22:31 +0200

    40480

--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# Extend the built-in user model with extra fields.
+class Profile(models.Model):
+    # One-to-One link with django's default User.
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+    # Extra fields for profile.
+    phone_number = models.CharField(max_length = 15, blank = True, null = True)
+
+    def __str__(self):
+        # Display username when looking at this object in admin panel.
+        return self.user.username
