00fc3c0ea8143185552af0b0cc138bffa8dded04
chifatso <156987629+chifatso@users.noreply.github.com>
2025-09-08 11:21:35 +0200

    43233

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Menu
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Item
+        model = Menu
         fields = '__all__'
