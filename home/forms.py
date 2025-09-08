708a7d7e34903ed90d15185098664960fc47ac85
chifatso <156987629+chifatso@users.noreply.github.com>
2025-09-08 15:51:42 +0200

    43230

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Feedback
+from .models import ContactSubmission
 
 class FeedbackForm(forms.ModelForm):
     class Meta:
@@ -7,4 +8,14 @@ class FeedbackForm(forms.ModelForm):
         fields = ['name', 'email', 'comments'] #Fields to display
         widgets = {
             'comments':forms.Textarea(attrs={'rows':4, 'placeholder':'Write your feedback here.'})
+        }
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactSubmission
+        fields = ['name', 'email'] #Only show these two fields to user
+        #Add bootstrap styling
+        widgets = {
+            'name':forms.TextInput(attrs = {'class':'form-control', 'placeholder':'Enter your name'}),
+            'email':forms.EmailInput(attrs = {'class':'form-control', 'placeholder':'Enter your email'}),
         }
