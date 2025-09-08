85066f93edbc96293b412524bd0c7467a67d87e8
chifatso <156987629+chifatso@users.noreply.github.com>
2025-09-08 16:01:00 +0200

    43230

--- home/views.py
+++ home/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from .models import Restaurant
 from django.db.utils import DatabaseError #to handle DB-specific error
 from .serializers import MenuItemSerializer
+from .forms import ContactForm
 
 
 # Create your views here.
@@ -21,11 +22,18 @@ def homepage(request):
         restaurant_phone = settings.restaurant_phone
 
     restaurant_address = getattr(settings, "RESTAURANT_ADDRESS", "Address not available")
+    #Initialize the form
+    form = ContactForm(request.POST or None)
+    #If form is submitted and valid->save to DB
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect("home") #refresh page after saving
 
     return render(request, "home/index.html",{
         "restaurant_name":restaurant_name,
         "restaurant_phone":restaurant_phone,
-        "restaurant_address":restaurant_address
+        "restaurant_address":restaurant_address,
+        "form":form, #Pass form to the template
     })
 
 def about(request):
