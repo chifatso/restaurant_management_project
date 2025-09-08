eebdbe92495b1af5fb286dacc249d860ac3b9279
chifatso <156987629+chifatso@users.noreply.github.com>
2025-09-08 15:37:02 +0200

    43230

--- home/models.py
+++ home/models.py
@@ -9,3 +9,16 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"Feedback from {self.name or 'Anonymous'}"
+
+class ContactSubmission(models.Model):
+    # Stores user's name
+    name = models.CharField(max_length = 100)
+
+    # Stores user's email
+    email = models.EmailField()
+
+    # Timestamp of submission
+    submitted_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
