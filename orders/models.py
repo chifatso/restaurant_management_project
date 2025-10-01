58578e24c1b1cd10b5101de4470eb832cc81149d
chifatso <156987629+chifatso@users.noreply.github.com>
2025-10-01 11:17:45 +0200

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -7,6 +7,7 @@ class Order(models.Model):
     customer_name = models.CharField(max_length = 100) #Using CharField for limited character length
     menu_item = models.ForeignKey(Menu, on_delete = models.CASCADE) #If Menu is deleted orders are deleted too.
     quantity = models.PositiveIntegerField(default = 1)
+    status = models.ForeignKey(OrderStatus, on_delete = models.SET_NULL, null = True) # Links the order model to order status model
     created_at = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
@@ -18,5 +19,6 @@ class OrderStatus(models.Model):
     def __str__(self):
         return self.name
     
+
 
 
