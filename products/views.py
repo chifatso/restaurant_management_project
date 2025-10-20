f9b4dfbb854e9f74cda07524819c2e70d6b7b82f
chifatso <156987629+chifatso@users.noreply.github.com>
2025-10-20 11:55:42 +0200

    Update views.py

--- products/views.py
+++ products/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import MenuItemSerializer
 from .models import Menu
+from rest_framework import viewsets, filters
+from rest_framework.pagination import PageNumberPagination
 
 '''
 NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
@@ -30,5 +32,27 @@ def menu_page(request):
 
     #Pass items to the template
     return render(request, "products/menu.html", {"menu_items":menu_items})
+class MenuItemPagination(PageNumberPagination):
+    """
+    Pagination class for Menu Items.
+    Limits the number of items returned per page.
+    """
+    page_size = 10  # Return 10 items per page
+    page_size_query_param = 'page_size'  # Allow clients to override this value
+
+class MenuItemSearchViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint to search for menu items by name.
+    Supports query parameter ?search=<term>
+    Example: /api/menu/search/?search=pizza
+    """
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    pagination_class = MenuItemPagination
+
+    # Enables filtering/searching functionality
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']  # Search by 'name' field (case-insensitive)
+
 
 
