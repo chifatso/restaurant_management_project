f49bc93e964b4637bda64bb636392985c865f08a
chifatso <156987629+chifatso@users.noreply.github.com>
2025-10-20 11:56:28 +0200

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("menu/", views.menu_page, name = "menu",)
+    path("menu/", views.menu_page, name = "menu",),
+    path('api/', include('products.urls')),
+
     
+
 ]
