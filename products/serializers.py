0048ec066d40c6c2148299a005a7ff6e3ccf231c
chifatso <156987629+chifatso@users.noreply.github.com>
2025-10-20 13:37:49 +0200

    Update serializers.py

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Menu
+from .models import MenuItem
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Menu
+        model = MenuItem
         fields = '__all__'
+    def validate_price(self, value):
+        """
+        Custom validation to ensure the price is positive.
+        """
+        if value <= 0:
+            raise serializers.ValidationError("Price must be greater than zero.")
+        return value
+
